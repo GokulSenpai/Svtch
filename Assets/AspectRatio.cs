a0b3d214d5bc78cccfa4654816162e6cb394093c
Goku <47669557+GokulSenpai@users.noreply.github.com>
2021-04-12 00:05:01 +0530

    PS1 style graphics, Improved Layout, Changed Aspect Ratio

---
+++ Assets/AspectRatio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRatio : MonoBehaviour
+{
+    public Vector2 aspectRatio;
+    public bool fullscreen;
+
+    private void Start()
+    {
+        SetRatio(aspectRatio.x, aspectRatio.y);
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void SetRatio(float w, float h)
+    {
+        if (Screen.width / (float)Screen.height > w / h)
+        {
+            Screen.SetResolution((int)(Screen.height * (w / h)), Screen.height, fullscreen);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.width, (int)(Screen.width * (h / w)), fullscreen);
+        }
+    }
+}
